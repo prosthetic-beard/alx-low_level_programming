92c692d7276472137ccc1972890fa445aeff6a01
Adebisi Ayodeji <hardeybeseajabson@gmail.com>
2022-06-09 20:28:54 +0100

    Persistent bugs need to be squashed

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-int positive_or_negative(int n);
+void positive_or_negative(int n);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
